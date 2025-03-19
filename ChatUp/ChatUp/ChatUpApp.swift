b174f9ace36b8dc52e04f9da1940f2359e66d703
Ricardo Massaki <ricardo29puruba@gmail.com>
2025-03-19 15:57:42 -0300

    Adicionando projeto ChatUp no Git

---
+++ ChatUp/ChatUp/ChatUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatUpApp.swift
+//  ChatUp
+//
+//  Created by Ricardo Massaki on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
