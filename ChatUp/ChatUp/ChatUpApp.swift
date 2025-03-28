9a4a0bfc6ad0be511d0d9a1156fc7f51262221f3
Ricardo Massaki <ricardo29puruba@gmail.com>
2025-03-28 14:41:29 -0300

    initial Commit

---
+++ ChatUp/ChatUp/ChatUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatUpApp.swift
+//  ChatUp
+//
+//  Created by Ricardo Massaki on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatUp/ChatUpTests/ChatUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatUpTests.swift
+//  ChatUpTests
+//
+//  Created by Ricardo Massaki on 28/03/25.
+//
+
+import Testing
+@testable import ChatUp
+
+struct ChatUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
